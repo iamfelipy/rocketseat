b3beb7b2f8eaf576ad22be6829fd5f7599d5b4b4
Felipy Camargo <felipy81br@gmail.com>
2021-10-25 10:51:25 -0300

    template engine ejs importei no serverjs,  adaptei as rotas para o ejs, mudei as extensoes dos arquivos html para ejs, transformei o header em componente e enviei valores personalizados, atualizei o header

--- discovery/jobsCalc/src/routes.js
+++ discovery/jobsCalc/src/routes.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const routes = express.Router();
 
-const basePath = __dirname + "/views";
+//antes do ejs, por padrão ejs já usa a pasta src/view
+const views = __dirname + "/views/";
+
+const profile = {
+    name: "Felipy",
+    avatar: "https://avatars.githubusercontent.com/u/50622611?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 8,
+    "vacation-per-year": 4
+}
 
 //request, response
-routes.get('/', (req, res) => res.sendFile(basePath+"/index.html"));
-routes.get('/job', (req, res) => res.sendFile(basePath + "/job.html"));
-routes.get('/job/edit', (req, res) => res.sendFile(basePath + "/job-edit.html"));
-routes.get('/profile', (req, res) => res.sendFile(basePath + "/profile.html"));
+//tambem não preciso dizer o tipo de extensao para o ejs ele já sabe que a saida é html
+// routes.get('/', (req, res) => res.sendFile(basePath+"/index.html"));
+routes.get('/', (req, res) => res.render(views+"index"));
+routes.get('/job', (req, res) => res.render(views+"job"));
+routes.get('/job/edit', (req, res) => res.render(views+"job-edit"));
+routes.get('/profile', (req, res) => res.render(views+"profile", {profile}));
 
 module.exports = routes;
--- discovery/jobsCalc/src/server.js
+++ discovery/jobsCalc/src/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const server = express();
 const routes = require("./routes");
 
+//set view engine
+server.set("view engine", "ejs");
+
 //habilitar arquivos statics
 server.use(express.static("public"));
 
