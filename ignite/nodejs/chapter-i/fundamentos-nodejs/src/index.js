5f4c9b85281409b1ec735edcebc0bf2232f73d7f
Felipy Camargo <felipy81br@gmail.com>
2021-11-20 15:56:28 -0300

    fundamentos-nodejs -http verbs with express

--- ignite/nodejs/chapter-i/fundamentos-nodejs/src/index.js
+++ ignite/nodejs/chapter-i/fundamentos-nodejs/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 /*
     HTTP VERBS
     GET - Buscar uma informação dentro do servidor
@@ -9,10 +11,34 @@ const app = express();
     PUT - Alterar uma informação no servidor
     PATCH - Alterar uma informação especifica
     DELETE - Deletar uma informação no servidor
+
+    .send
 */
 
-app.get("/", (request, response)=>{
-    response.json({message: "Oi, você é bilhonário, você acredita nisso?"})
+app.get("/courses", (request, response)=>{
+    const query = request.query;
+    console.log(query);
+    return response.json(["curso 1","curso 2","curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+	const body = request.body;
+	console.log(body);
+    return response.json(["curso 1","curso 2","curso 3","curso 4"]);
+});
+
+app.put("/courses/:id", (request, response)=>{
+    const {id} = request.params;
+    console.log(id);
+    return response.json(["curso 6","curso 2","curso 3","curso 4"]);
+});
+
+app.patch("/courses/:id",(request, response)=>{
+    return response.json(["curso 6","curso 7","curso 3","curso 4"]);
+});
+
+app.delete("/courses/:id",(request, response)=>{
+    return response.json(["curso 6","curso 7","curso 4"]);
 });
 
 app.listen(3333);
