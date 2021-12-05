85bdda7ca50048f72633b2d770e15f1bf9194500
Felipy Camargo <felipy81br@gmail.com>
2021-12-05 17:53:33 -0300

    Módulo S.O.L.I.D final

---
+++ ignite/nodejs/chapter-ii/iniciando-a-api/rentalx/src/services/CreateCategoryService.ts
@@ -0,0 +1,32 @@
+
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+/*
+ [] - Definir o tipo de retorno
+ [] - Alterar o retorno de erro
+ [] - Acessar o repositorio
+ [] - Retornar algo
+*/
+
+class CreateCategoryService {
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    execute({ description, name }: IRequest): void {
+        const categoryAlreadyExists =
+            this.categoriesRepository.findByName(name);
+
+        if (categoryAlreadyExists) {
+            throw new Error("Category already exists!");
+            // return response.status(400).json({ error: "Category already exists!" });
+        }
+
+        this.categoriesRepository.create({ name, description });
+    }
+}
+
+export { CreateCategoryService };
