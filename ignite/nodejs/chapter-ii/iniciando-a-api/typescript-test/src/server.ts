ce939ecb8676c22c6e858efe7c7f452b19ec8b01
Felipy Camargo <felipy81br@gmail.com>
2021-12-19 11:34:19 -0300

    desafio 1 e 2 do nodejs chapter-ii

--- ignite/nodejs/chapter-ii/iniciando-a-api/typescript-test/src/server.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {createCourse} from './routes';
-
-const app = express();
-
-app.get('/', createCourse);
-
-app.listen(4893,()=>console.log("Server rodando."));
